088cfe8336987299b185f403ddbe91fc97ffce09
rcedi1012 <rocedi@utexas.edu>
2025-08-18 03:49:27 -0500

    dying

---
+++ script.js
@@ -0,0 +1,26 @@
+//global variables: img names
+let bell, bell1, bell2, bellding, body, curtains, desk, facen, facet,
+hat, head, middlebell, paw1, paw2, pupil1, pupil2, sclera1, scelra2, sleepy,
+smug, topcurtain;
+
+let aspectH;
+
+function preload() {
+    bell = loadImage("bell.png");
+    bell1 = loadImage("bell1.png");
+}
+
+function setup() {
+    canvas = createCanvas(windowWidth, (7 * windowWidth)/8);
+    canvas.parent('container');
+}
+
+function draw() {
+    background(100)
+    image(bell, 0, 0, windowWidth * 0.1, ((7 * windowWidth)/8) * 0.1);
+    image(bell1,0,0)
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, (7 * windowWidth)/8);
+}
