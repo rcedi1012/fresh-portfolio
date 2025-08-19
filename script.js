29ea51beaa8ea82d4ae57624b458c68b8c2c2980
rcedi1012 <rocedi@utexas.edu>
2025-08-18 20:50:02 -0500

    working with classes

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 //global variables: img names
-let bell, bell1, bell2, bellding, body, curtains, desk, facen, facet,
+let bellimg, bell1, bell2, bellding, body, curtains, desk, facen, facet,
 hat, head, middlebell, paw1, paw2, pupil1, pupil2, sclera1, scelra2, sleepy,
 smug, topcurtain;
 
-let aspectH;
+let bell = new Receptionbell(0,0,bellimg);
+
 
 function preload() {
-    bell = loadImage("bell.png");
+    bellimg = loadImage('bell.png');
     bell1 = loadImage("bell1.png");
+    bellding = loadImage("bellding.png");
 }
 
 function setup() {
     canvas = createCanvas(windowWidth, (7 * windowWidth)/8);
     canvas.parent('container');
+    
 }
 
 function draw() {
     background(100)
-    image(bell, 0, 0, windowWidth, ((7 * windowWidth)/8));
     image(bell1,0,0, windowWidth, ((7 * windowWidth)/8))
+    bell.show();
+    
 }
 
 function windowResized() {
